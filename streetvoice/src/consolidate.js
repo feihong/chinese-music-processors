2b32e0b868fe0179bfdd41d60109f90ae42eaca0
Feihong Hsu <feihong.hsu@gmail.com>
2020-05-29 17:35:12 -0500

    Get song metadata from json instead of html

--- streetvoice/src/consolidate.js
+++ streetvoice/src/consolidate.js
@@ -2,92 +2,100 @@
 Combine metadata, cover art, and audio data into complete .m4a files. Also
 apply track gain to balance loudness levels.
 */
-const fs = require('fs')
-const childProcess = require('child_process')
-const sanitize = require("sanitize-filename")
-const paths = require('./path.config')
+const fs = require("fs");
+const childProcess = require("child_process");
+const sanitize = require("sanitize-filename");
+const paths = require("./path.config");
 
 // Convert .ts to .m4a
 function convertToM4a(song, outputFile) {
-  const tsFile = `${paths.assetsDir}/${song.id}.ts`
+  const tsFile = `${paths.assetsDir}/${song.id}.ts`;
   if (!fs.existsSync(tsFile)) {
-    return
+    return;
   }
 
   // Convert and add metadata in one step
   childProcess.spawnSync(
-    'ffmpeg',
+    "ffmpeg",
     [
-      '-y',   // overwrite if file already exists
-      '-i', tsFile,
-      '-metadata', `title=${song.title}`,
-      '-metadata', `artist=${song.artist}`,
-      '-metadata', `comment=${song.url}`,
-      '-metadata', `lyrics=${song.lyrics}`,
-      '-metadata', 'genre=流行 Pop',
-      '-vn',                // ignore video
-      '-c:a', 'libfdk_aac', // use best encoder
-      '-vbr', '4',          // use high quality
+      "-y", // overwrite if file already exists
+      "-i",
+      tsFile,
+      "-metadata",
+      `title=${song.title}`,
+      "-metadata",
+      `artist=${song.artist}`,
+      "-metadata",
+      `album=${song.album}`,
+      "-metadata",
+      `comment=${song.url}`,
+      "-metadata",
+      `lyrics=${song.lyrics}`,
+      "-metadata",
+      `genre=${song.genre}`,
+      "-metadata",
+      `year=${song.year}`,
+      "-vn", // ignore video
+      "-c:a",
+      "libfdk_aac", // use best encoder
+      "-vbr",
+      "4", // use high quality
       outputFile,
     ],
-    { stdio: 'inherit' },
-  )
+    { stdio: "inherit" }
+  );
 
   // Balance loudness levels
   childProcess.spawnSync(
-    'aacgain',
+    "aacgain",
     [
-      '-r',  // apply Track gain automatically (all files set to equal loudness)
-      '-k',  // automatically lower Track/Album gain to not clip audio
+      "-r", // apply Track gain automatically (all files set to equal loudness)
+      "-k", // automatically lower Track/Album gain to not clip audio
       outputFile,
     ],
-    { stdio: 'inherit' },
-  )
+    { stdio: "inherit" }
+  );
 }
 
 function addCoverArt(song, outputFile) {
   if (!fs.existsSync(song.imageFile)) {
-    return
+    return;
   }
 
   // If the image is webp, convert it to png since AtomicParsley cannot handle
   // webp
-  if (song.imageFile.endsWith('.webp')) {
-    const pngFile = `${paths.assetsDir}/${song.imageId}.png`
+  if (song.imageFile.endsWith(".webp")) {
+    const pngFile = `${paths.assetsDir}/${song.imageId}.png`;
     childProcess.spawnSync(
-      'ffmpeg',
+      "ffmpeg",
       [
-        '-y',   // overwrite if file already exists
-        '-i', song.imageFile,
+        "-y", // overwrite if file already exists
+        "-i",
+        song.imageFile,
         pngFile,
       ],
-      { stdio: 'inherit' }
-    )
-    song.imageFile = pngFile
+      { stdio: "inherit" }
+    );
+    song.imageFile = pngFile;
   }
 
   // We do not add metadata in this step because AtomicParsley is notoriously
   // bad at adding lyrics
   childProcess.spawnSync(
-    'AtomicParsley',
-    [
-      outputFile,
-      '--artwork', song.imageFile,
-      '--overWrite',
-    ],
-    { stdio: 'inherit' }
-  )
+    "AtomicParsley",
+    [outputFile, "--artwork", song.imageFile, "--overWrite"],
+    { stdio: "inherit" }
+  );
 }
 
-const songs = require(paths.songsFile)
+const songs = require(paths.songsFile);
 
 for (const song of songs) {
   console.log(song.title);
 
-  const name = sanitize(`${song.artist}  ${song.title}.m4a`)
-  const outputFile = `${paths.outputDir}/${name}`
+  const name = sanitize(`${song.artist}  ${song.title}.m4a`);
+  const outputFile = `${paths.outputDir}/${name}`;
 
-  convertToM4a(song, outputFile)
-  addCoverArt(song, outputFile)
+  convertToM4a(song, outputFile);
+  addCoverArt(song, outputFile);
 }
-
--- streetvoice/src/generate-metadata.js
+++ streetvoice/src/generate-metadata.js
@@ -1,51 +1,48 @@
 /*
 Scrape playlist pages and convert the track metadata within to json
 */
-const fs = require('fs')
-const cheerio = require('cheerio')
-const Database = require('better-sqlite3')
-const paths = require('./path.config')
+const fs = require("fs");
+const Database = require("better-sqlite3");
+const paths = require("./path.config");
 
-const db = new Database('dumpfile.db', { fileMustExist: true })
+const db = new Database("dumpfile.db", { fileMustExist: true });
 
 function prepare(whereClause) {
-  return db.prepare("SELECT * FROM dump WHERE " + whereClause)
+  return db.prepare("SELECT * FROM dump WHERE " + whereClause);
 }
 
+const imageIdRe = /\/([a-zA-Z0-9]+)[.](?:jpg|jpeg|png)/i;
+
 function* getSongs() {
-  const pages =
-    prepare("path LIKE '%/playlists/%' AND content_type LIKE 'text/html%'")
-  for (const page of pages.iterate()) {
-    const playlistRe = /playlists\/([0-9]+)\//
-    const match = page.path.match(playlistRe)
-    if (!match) {
-      continue
-    }
-    const playlistId = match[1]
-    fs.writeFileSync(`./assets/${playlistId}.html`, page.data)
+  const stmt = prepare(
+    "path LIKE '%/songs/%song_ids=%' AND content_type LIKE 'application/json%'"
+  );
+  let count = 0;
+  for (const row of stmt.iterate()) {
+    count += 1;
+    fs.writeFileSync(`./assets/songs_${count}.json`, row.data);
 
-    const $ = cheerio.load(page.data)
-    yield* $('#item_box_list .item_box').map((i, el) => {
-      const item = $(el)
-      const anchor = item.find('h5 > a')
-      const imageUrl = item.find('img').attr('src')
-      const imageIdRe = /\/([a-zA-Z0-9]+)[.](?:jpg|jpeg|png)/i
+    const data = JSON.parse(row.data.toString());
 
+    yield* data.results.map((result) => {
       return {
-        id: item.find('.btn-play').data('id'),
-        title: item.find('.work-item-info > h4').text().trim(),
-        artist: anchor.text().trim(),
-        url: 'https://streetvoice.com' + anchor.attr('href'),
-        imageUrl,
-        imageId: imageUrl.match(imageIdRe)[1],
-      }
-    }).get()
+        id: result.id,
+        title: result.name,
+        artist: result.user.nickname,
+        url: `https://streetvoice.com/${result.user.username}/songs/${result.id}/`,
+        imageUrl: result.image,
+        imageId: result.image.match(imageIdRe)[1],
+        album: result.album ? result.album.name : "",
+        genre: result.genre,
+        year: new Date(result.created_at).getFullYear(),
+      };
+    });
   }
 }
 
-const songs = [...getSongs()]
-fs.writeFileSync(paths.songsFile, JSON.stringify(songs, null, 2))
+const songs = [...getSongs()];
+fs.writeFileSync(paths.songsFile, JSON.stringify(songs, null, 2));
 
 songs.forEach((song, i) => {
-  console.log(`${i + 1}. ${song.title}`)
-})
+  console.log(`${i + 1}. ${song.title}`);
+});
