117f95733a1282dcebccbcf4a2cdc48f586e6b28
Feihong Hsu <feihong.hsu@gmail.com>
2021-01-10 09:29:49 -0600

    Remove douban task

--- tasks.py
+++ tasks.py
@@ -3,13 +3,6 @@ from invoke import task
 
 
 @task
-def douban(ctx, input_file='input.json'):
-    "Process songs from Douban Music"
-    from douban import process
-    process(input_file)
-
-
-@task
 def m4a(ctx):
     "Process m4a files"
     from m4a_file import process
