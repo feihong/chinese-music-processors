03230aa9be8e8eed8f93b588e1839e77e8f5ea4b
Feihong Hsu <feihong.hsu@gmail.com>
2021-12-15 08:36:47 -0600

    Migrate playlist script from Python to Coconut

--- youtube/TODO.md
+++ youtube/TODO.md
@@ -1,3 +1,4 @@
 # TODO
 
 - [x] Support trimming audio file when extracting it
+- [ ] Migrate from Python to Coconut
--- youtube/playlist.py
+++
@@ -1,9 +0,0 @@
-import sys
-from main import process_playlist
-
-try:
-  url = sys.argv[1]
-except:
-  url = None
-
-process_playlist(url)
