3b72a8f346c81edc545fc32189381679df5281d9
Feihong Hsu <feihong.hsu@gmail.com>
2022-02-26 16:12:19 -0600

    Get youtube playlist from .env file

--- move.py
+++ move.py
@@ -5,20 +5,20 @@ from datetime import datetime
 import shutil
 from dotenv import dotenv_values
 
-config = dotenv_values('.env')
+target_dir = Path(dotenv_values('.env')['target_dir']).expanduser()
 
 today = datetime.today()
-target_dir = Path(config['target_dir']).expanduser() / f'{today.year}-{today.month:02}'
-if not target_dir.exists():
-  target_dir.mkdir()
+month_dir = target_dir / f'{today.year}-{today.month:02}'
+if not month_dir.exists():
+  month_dir.mkdir()
 
 processed_songs = list(Path('.').glob('**/output/*.m4a'))
 for file in processed_songs:
   print(file)
 
-result = input(f'\nMove these {len(processed_songs)} files to {target_dir}? ')
+result = input(f'\nMove these {len(processed_songs)} files to {month_dir}? ')
 if result.strip().lower() == 'n':
   sys.exit(0)
 
 for file in processed_songs:
-  shutil.move(file, target_dir)
+  shutil.move(file, month_dir)
